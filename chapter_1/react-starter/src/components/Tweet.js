2134dc0c8fe0c6f428e13c4aa82f1142cf92d700
Cameron Moore <cam@cameronlpmoore.com>
2020-11-16 15:43:50 -0800

    starting up

---
+++ chapter_1/react-starter/src/components/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tweet() {
+    return (
+        <div>
+            <h1>This is a tweet</h1>
+            <button>Like</button>
+            <button>Delete</button>
+        </div>
+    );
+}
+
+export default Tweet;
