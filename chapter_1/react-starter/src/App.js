940cfa916bace972d7838ec0ffbf809d0e2c710a
Cameron Moore <cam@cameronlpmoore.com>
2020-11-17 09:26:44 -0800

    refactors Tweet

--- chapter_1/react-starter/src/App.js
+++ chapter_1/react-starter/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Tweet from './components/Tweet';
+import CreateTweet from './components/CreateTweet';
 
 function App() {
+  const name = "Pancake";
+  let message = "this is the raw tweet literal";
+
   return (
     <div>
       <h1>456</h1>
-      <Tweet />
+      <CreateTweet />
+      <Tweet name={name} message={message}/>
     </div>
   );
 }
